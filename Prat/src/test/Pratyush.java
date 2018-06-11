0f96ebae93000074117d3ec22e601f4102e9f101
HOME <HOME@DESKTOP-5SKJAR1>
2018-06-11 16:55:08 +0530

    hii servlet

---
+++ Prat/src/test/Pratyush.java
@@ -0,0 +1,11 @@
+package test;
+
+public class Pratyush {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("...........................................");
+
+	}
+
+}
