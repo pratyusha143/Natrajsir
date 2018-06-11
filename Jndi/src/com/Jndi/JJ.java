6c1e3876e9d72f9149aa7de97541b34c00b3fbfc
HOME <HOME@DESKTOP-5SKJAR1>
2018-06-11 17:35:40 +0530

    Hii its Developer

---
+++ Jndi/src/com/Jndi/JJ.java
@@ -0,0 +1,11 @@
+package com.Jndi;
+
+public class JJ {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("duyikasio...........ukgiluas");
+
+	}
+
+}
